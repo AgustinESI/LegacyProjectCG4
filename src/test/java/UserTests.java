a668e903ce42ccfd90c8902162659049a2f5d92d
david <david@DAVID-PC>
2020-10-25 18:09:55 +0100

    Add test cases

--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -21,5 +21,47 @@ public class UserTests {
 		assertEquals(aux.getPassword(), user.getPassword());
 		assertEquals(aux.getDni(), user.getDni());
 
+		
+		User a1 = new User("p1", "p1","12356879F");
+		
+		aux = userController.createUser(a1);
+
+		assertEquals(aux.getName(), a1.getName());
+		assertEquals(aux.getPassword(), a1.getPassword());
+		assertEquals(aux.getDni(), a1.getDni());
+		
+		
+		User a2 = new User("p2", "p2","12356870F");
+		
+		aux = userController.createUser(a2);
+
+		assertEquals(aux.getName(), a2.getName());
+		assertEquals(aux.getPassword(), a2.getPassword());
+		assertEquals(aux.getDni(), a2.getDni());
+		
+		User a3 = new User("p3", "p3","12356878F");
+		
+		aux = userController.createUser(a3);
+
+		assertEquals(aux.getName(), a3.getName());
+		assertEquals(aux.getPassword(), a3.getPassword());
+		assertEquals(aux.getDni(), a3.getDni());
+		
+		User a4 = new User("p4", "p4","12356877F");
+		
+		aux = userController.createUser(a4);
+
+		assertEquals(aux.getName(), a4.getName());
+		assertEquals(aux.getPassword(), a4.getPassword());
+		assertEquals(aux.getDni(), a4.getDni());
+		
+		User a5 = new User("p5", "p5","12356875F");
+		
+		aux = userController.createUser(a5);
+
+		assertEquals(aux.getName(), a5.getName());
+		assertEquals(aux.getPassword(), a5.getPassword());
+		assertEquals(aux.getDni(), a5.getDni());
+		
 	}
 }
